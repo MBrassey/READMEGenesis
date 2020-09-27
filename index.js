dbc963948c5c64f856d5c5421d3d9bcb5615a51b
luc1dLife <luc1d_life@protonmail.com>
2020-09-26 22:23:06 -0600

    Initial Files

---
+++ index.js
@@ -0,0 +1,21 @@
+const inquirer = require("inquirer");
+//const { writeFile, copyFile } = require("./utils/generate-readme.js");
+//const generatePage = require("./src/readme-template");
+const arg = process.argv[2];
+const version = "0.0.1";
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    console.log("started");
+}
+
+// function call to initialize program
+init();
