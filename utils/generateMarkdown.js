df1f311e8797f9f1f07cccd2b57597cc9a980a1d
luc1dLife <luc1d_life@protonmail.com>
2020-09-30 22:15:54 -0600

    License

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,37 @@
 const fs = require("fs");
+// "Apache2.0", "GPLv3.0", "MIT", "BSD2", "BSD3", "Boost", "CreativeCommons", "Eclipse2.0", "AGPL", "GPLv2.0", "LGPLv2.1", "Mozilla2.0", "TheUnlicense"
+const licenseDetail = license => {
+  if (license === "Apache2.0") {
+    return '    A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+  } else if (license === "GPLv3.0") {
+    return '    Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.';
+  } else if (license === "MIT") {
+    return '    A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+  } else if (license === "BSD2") {
+    return '    A permissive license that comes in two variants, the BSD 2-Clause and BSD 3-Clause. Both have very minute differences to the MIT license.';
+  } else if (license === "BSD3") {
+    return '    A permissive license similar to the BSD 2-Clause License, but with a 3rd clause that prohibits others from using the name of the project or its contributors to promote derived products without written consent.';
+  } else if (license === "Boost") {
+    return '    A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+  } else if (license === "CreativeCommons") {
+    return '    The Creative Commons CC0 Public Domain Dedication waives copyright interest in a work you,ve created and dedicates it to the world-wide public domain. Use CC0 to opt out of copyright entirely and ensure your work has the widest reach. As with the Unlicense and typical software licenses, CC0 disclaims warranties. CC0 is very similar to the Unlicense.';
+  } else if (license === "Eclipse2.0") {
+    return '    This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones.';
+  } else if (license === "AGPL") {
+    return '    Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.';
+  } else if (license === "GPLv2.0") {
+    return '    The GNU GPL is the most widely used free software license and has a strong copyleft requirement. When distributing derived works, the source code of the work must be made available under the same license. There are multiple variants of the GNU GPL, each with different requirements.';
+  } else if (license === "LGPLv2.1") {
+    return '    Primarily used for software libraries, the GNU LGPL requires that derived works be licensed under the same license, but works that only link to it do not fall under this restriction. There are two commonly used versions of the GNU LGPL.';
+  } else if (license === "Mozilla2.0") {
+    return '    Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.';
+  } else if (license === "TheUnlicense") {
+    return '    A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.';
+  }
+return `
+    No License. . .
+`
+}
 
 const variableScreenshot1 = data => {
   if (!data.screenshotURL1) {
@@ -121,6 +154,11 @@ ${variableScreenshot2(data)}
 
 #### Questions
 Contact me at [${data.contact}](mailto:${data.contact}) with any questions or comments. 
+
+#### License
+${data.title} is published under the ${data.license} license.
+
+${licenseDetail(data.license)}
 `;
 }
 
