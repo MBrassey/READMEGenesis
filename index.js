e30425c0bb21e9ab55bd6d6816f191be869ecf8a
luc1dLife <luc1d_life@protonmail.com>
2020-10-10 22:38:26 -0600

    Linted

--- index.js
+++ index.js
@@ -4,156 +4,185 @@ const arg = process.argv[2];
 const version = "0.0.1";
 
 const promptUser = (readmeData) => {
-        readmeData = [];
+  readmeData = [];
 
-    console.log(`
+  console.log(`
     [ README Generator ]
   `);
-    return inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "gitHub",
-                message: "Enter your GitHub username:",
-                validate: (username) => {
-                    if (username) {
-                        return true;
-                    } else {
-                        console.log("Enter your GitHub username!");
-                        return false;
-                    }
-                },
-            },
-            {
-                type: "input",
-                name: "title",
-                message: "Enter project title:",
-                validate: (title) => {
-                    if (title) {
-                        return true;
-                    } else {
-                        console.log("Enter a title for the project!");
-                        return false;
-                    }
-                },
-            },
-            {
-                type: "input",
-                name: "description",
-                message: "Provide a description of the project:",
-                validate: (titleInput) => {
-                    if (titleInput) {
-                        return true;
-                    } else {
-                        console.log("Enter a description for the project!");
-                        return false;
-                    }
-                },
-            },
-            {
-                type: "checkbox",
-                name: "languages",
-                message: "What did you build this project with? (Check all that apply)",
-                choices: ["HTML", "CSS", "JavaScript", "ES6", "jQuery", "Express", "Bootstrap", "Node", "React", "Web3", "Solidity", "Bash", "Python", "AutoIt", "PowerShell"],
-            },
-            {
-                type: "checkbox",
-                name: "requirements",
-                message: "Check any requirements or dependencies from the list:",
-                choices: ["node", "npm", "react", "Ganache"],
-            },
-            {
-                type: "confirm",
-                name: "installation",
-                message: "Include installation section?",
-                default: false,
-            },
-            {
-                type: "input",
-                name: "installationInstructions",
-                message: "Provide installation instructions ( , for new line ):",
-                when: ({ installation }) => installation,
-            },
-            {
-                type: "confirm",
-                name: "usage",
-                message: "Include usage instructions?",
-                default: false,
-            },
-            {
-                type: "input",
-                name: "usageInstructions",
-                message: "Provide usage instructions ( , for new line ):",
-                when: ({ usage }) => usage,
-            },
-            {
-                type: "confirm",
-                name: "screenshot",
-                message: "Include screenshot?",
-                default: false,
-            },
-            {
-                type: "input",
-                name: "screenshotURL1",
-                message: "Provide media URL:",
-                when: ({ screenshot }) => screenshot,
-            },
-            {
-                type: "confirm",
-                name: "screenshot2",
-                message: "Would you like to enter another screenshot?",
-                default: false,
-                when: ({ screenshotURL1 }) => screenshotURL1,
-            },
-            {
-                type: "input",
-                name: "screenshotURL2",
-                message: "Provide media URL:",
-                when: ({ screenshot2 }) => screenshot2,
-            },
-            {
-                type: "input",
-                name: "contact",
-                message: "Provide contact e-mail address:",
-                validate: (emailAddress) => {
-                    if (emailAddress) {
-                        return true;
-                    } else {
-                        console.log("Enter your e-mail address!");
-                        return false;
-                    }
-                },
-            },
-            {
-                type: "confirm",
-                name: "tOc",
-                message: "Include table of contents?",
-                default: false,
-            },
-            {
-                type: "confirm",
-                name: "issues",
-                message: "Include list of the projects issues?",
-                default: false,
-            },
-            {
-                type: "input",
-                name: "issueTitles",
-                message: "Enter issue names ( separated by , ):",
-                when: ({ issues }) => issues,
-            },
-            {
-                type: "list",
-                name: "license",
-                message: "Under which license are you publishing the project?",
-                choices: ["Apache2.0", "GPLv3.0", "MIT", "BSD2", "BSD3", "Boost", "CC0_1.0_Universal", "Eclipse2.0", "AGPL", "GPLv2.0", "LGPLv2.1", "Mozilla2.0", "TheUnlicense"],
-            }
-        ])
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "gitHub",
+      message: "Enter your GitHub username:",
+      validate: (username) => {
+        if (username) {
+          return true;
+        } else {
+          console.log("Enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "Enter project title:",
+      validate: (title) => {
+        if (title) {
+          return true;
+        } else {
+          console.log("Enter a title for the project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project:",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Enter a description for the project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "ES6",
+        "jQuery",
+        "Express",
+        "Bootstrap",
+        "Node",
+        "React",
+        "Web3",
+        "Solidity",
+        "Bash",
+        "Python",
+        "AutoIt",
+        "PowerShell",
+      ],
+    },
+    {
+      type: "checkbox",
+      name: "requirements",
+      message: "Check any requirements or dependencies from the list:",
+      choices: ["node", "npm", "react", "Ganache"],
+    },
+    {
+      type: "confirm",
+      name: "installation",
+      message: "Include installation section?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "installationInstructions",
+      message: "Provide installation instructions ( , for new line ):",
+      when: ({ installation }) => installation,
+    },
+    {
+      type: "confirm",
+      name: "usage",
+      message: "Include usage instructions?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "usageInstructions",
+      message: "Provide usage instructions ( , for new line ):",
+      when: ({ usage }) => usage,
+    },
+    {
+      type: "confirm",
+      name: "screenshot",
+      message: "Include screenshot?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "screenshotURL1",
+      message: "Provide media URL:",
+      when: ({ screenshot }) => screenshot,
+    },
+    {
+      type: "confirm",
+      name: "screenshot2",
+      message: "Would you like to enter another screenshot?",
+      default: false,
+      when: ({ screenshotURL1 }) => screenshotURL1,
+    },
+    {
+      type: "input",
+      name: "screenshotURL2",
+      message: "Provide media URL:",
+      when: ({ screenshot2 }) => screenshot2,
+    },
+    {
+      type: "input",
+      name: "contact",
+      message: "Provide contact e-mail address:",
+      validate: (emailAddress) => {
+        if (emailAddress) {
+          return true;
+        } else {
+          console.log("Enter your e-mail address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "tOc",
+      message: "Include table of contents?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "issues",
+      message: "Include list of the projects issues?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "issueTitles",
+      message: "Enter issue names ( separated by , ):",
+      when: ({ issues }) => issues,
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Under which license are you publishing the project?",
+      choices: [
+        "Apache2.0",
+        "GPLv3.0",
+        "MIT",
+        "BSD2",
+        "BSD3",
+        "Boost",
+        "CC0_1.0_Universal",
+        "Eclipse2.0",
+        "AGPL",
+        "GPLv2.0",
+        "LGPLv2.1",
+        "Mozilla2.0",
+        "TheUnlicense",
+      ],
+    },
+  ]);
 };
 
 // Display Argument Data
 if (arg === "-h") {
-    console.log(`
+  console.log(`
     Usage: node index.js [ -h | -v | -l | -a ]
 
     [options]
@@ -162,28 +191,31 @@ if (arg === "-h") {
     -v          Show version.
     -l          Show license info.
     -a          What is READMEGenesis?
-`);} else if (arg === "-v") {
-    console.log("READMEGenesis Version: " + version);
+`);
+} else if (arg === "-v") {
+  console.log("READMEGenesis Version: " + version);
 } else if (arg === "-l") {
-    console.log("Licensed under the GNU General Public License v3.0");
+  console.log("Licensed under the GNU General Public License v3.0");
 } else if (arg === "-a") {
-    console.log("Menu driven command line utility written in Node.js to generate a professional README for new projects.");
+  console.log(
+    "Menu driven command line utility written in Node.js to generate a professional README for new projects."
+  );
 } else {
-// Initialize the App
-function init() {
+  // Initialize the App
+  function init() {
     promptUser()
-    .then((readmeData) => {
+      .then((readmeData) => {
         console.log(readmeData);
         return generateMarkdown(readmeData);
-    })
-    .then((markdown) => {
+      })
+      .then((markdown) => {
         //console.log(markdown);
         return writeFile(markdown);
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
-    });
-}
+      });
+  }
 
-init();
+  init();
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,100 +1,100 @@
 const fs = require("fs");
-const licenseDetail = license => {
+const licenseDetail = (license) => {
   if (license === "Apache2.0") {
-    return '> A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+    return "> A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
   } else if (license === "GPLv3.0") {
-    return '> Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.';
+    return "> Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
   } else if (license === "MIT") {
-    return '> A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+    return "> A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
   } else if (license === "BSD2") {
-    return '> A permissive license that comes in two variants, the BSD 2-Clause and BSD 3-Clause. Both have very minute differences to the MIT license.';
+    return "> A permissive license that comes in two variants, the BSD 2-Clause and BSD 3-Clause. Both have very minute differences to the MIT license.";
   } else if (license === "BSD3") {
-    return '> A permissive license similar to the BSD 2-Clause License, but with a 3rd clause that prohibits others from using the name of the project or its contributors to promote derived products without written consent.';
+    return "> A permissive license similar to the BSD 2-Clause License, but with a 3rd clause that prohibits others from using the name of the project or its contributors to promote derived products without written consent.";
   } else if (license === "Boost") {
-    return '> A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+    return "> A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
   } else if (license === "CC0_1.0_Universal") {
-    return '> The Creative Commons CC0 Public Domain Dedication waives copyright interest in a work you\'ve created and dedicates it to the world-wide public domain. Use CC0 to opt out of copyright entirely and ensure your work has the widest reach. As with the Unlicense and typical software licenses, CC0 disclaims warranties. CC0 is very similar to the Unlicense.';
+    return "> The Creative Commons CC0 Public Domain Dedication waives copyright interest in a work you've created and dedicates it to the world-wide public domain. Use CC0 to opt out of copyright entirely and ensure your work has the widest reach. As with the Unlicense and typical software licenses, CC0 disclaims warranties. CC0 is very similar to the Unlicense.";
   } else if (license === "Eclipse2.0") {
-    return '> This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones.';
+    return "> This commercially-friendly copyleft license provides the ability to commercially license binaries; a modern royalty-free patent license grant; and the ability for linked works to use other licenses, including commercial ones.";
   } else if (license === "AGPL") {
-    return '> Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.';
+    return "> Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.";
   } else if (license === "GPLv2.0") {
-    return '> The GNU GPL is the most widely used free software license and has a strong copyleft requirement. When distributing derived works, the source code of the work must be made available under the same license. There are multiple variants of the GNU GPL, each with different requirements.';
+    return "> The GNU GPL is the most widely used free software license and has a strong copyleft requirement. When distributing derived works, the source code of the work must be made available under the same license. There are multiple variants of the GNU GPL, each with different requirements.";
   } else if (license === "LGPLv2.1") {
-    return '> Primarily used for software libraries, the GNU LGPL requires that derived works be licensed under the same license, but works that only link to it do not fall under this restriction. There are two commonly used versions of the GNU LGPL.';
+    return "> Primarily used for software libraries, the GNU LGPL requires that derived works be licensed under the same license, but works that only link to it do not fall under this restriction. There are two commonly used versions of the GNU LGPL.";
   } else if (license === "Mozilla2.0") {
-    return '> Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.';
+    return "> Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.";
   } else if (license === "TheUnlicense") {
-    return '> A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.';
+    return "> A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.";
   }
-return `
+  return `
     No License. . .
-`
-}
+`;
+};
 
-const variableScreenshot1 = data => {
+const variableScreenshot1 = (data) => {
   if (!data.screenshotURL1) {
-    return '';
+    return "";
   }
-return `
+  return `
 [<p align="center"><img src="${data.screenshotURL1}">](https://${data.gitHub}.github.io/${data.title}/)
-`
-}
+`;
+};
 
-const variableScreenshot2 = data => {
+const variableScreenshot2 = (data) => {
   if (!data.screenshotURL2) {
-    return '';
+    return "";
   }
-return `
+  return `
 [<p align="center"><img src="${data.screenshotURL2}">](https://${data.gitHub}.github.io/${data.title}/)
-`
-}
+`;
+};
 
-const variableUsage = usageInstructions => {
+const variableUsage = (usageInstructions) => {
   if (!usageInstructions) {
-    return '';
+    return "";
   }
-  var str5 = usageInstructions + '';
-  var presentUsageInstructions = str5.split(",").join("\n"+"    ");
-return `
+  var str5 = usageInstructions + "";
+  var presentUsageInstructions = str5.split(",").join("\n" + "    ");
+  return `
 #### Usage
 
     ${presentUsageInstructions}
-`
-}
+`;
+};
 
-const variableInstallation = installationInstructions => {
+const variableInstallation = (installationInstructions) => {
   if (!installationInstructions) {
-    return '';
+    return "";
   }
-  var str4 = installationInstructions + '';
-  var presentInstallationInstructions = str4.split(",").join("\n"+"    ");
-return `
+  var str4 = installationInstructions + "";
+  var presentInstallationInstructions = str4.split(",").join("\n" + "    ");
+  return `
 #### Installation
 
     ${presentInstallationInstructions}
-`
-}
+`;
+};
 
-const variableRequirements = requirements => {
+const variableRequirements = (requirements) => {
   if (!requirements) {
-    return '';
+    return "";
   }
-  var str3 = requirements + '';
-  var presentRequirements = str3.split(",").join("\n"+"    ");
+  var str3 = requirements + "";
+  var presentRequirements = str3.split(",").join("\n" + "    ");
 
-return `
+  return `
 #### Requirements
 
     ${presentRequirements}
-`
-}
+`;
+};
 
-const variableToc = tOc => {
+const variableToc = (tOc) => {
   if (!tOc) {
-    return '';
+    return "";
   }
-return `
+  return `
 #### Table of Contents
 
 * [Requirements](#Requirements)
@@ -103,33 +103,43 @@ return `
 * [Screenshot(s)](#Screenshots)
 * [Questions](#Questions)
 * [License](#License)
-`
-}
+`;
+};
 
-const variableIssues = data => {
+const variableIssues = (data) => {
   if (!data.issueTitles) {
-    return '';
+    return "";
   }
   var str = data.issueTitles;
-  var issue = str.split(',');
+  var issue = str.split(",");
   var issueLines = [];
   for (var i = 1; i < issue.length; i++) {
-issueLines.push("- [x] ["+ issue[i] +"](https://github.com/"+ data.gitHub +"/"+ data.title +"/issues/"+ i +")");
+    issueLines.push(
+      "- [x] [" +
+        issue[i] +
+        "](https://github.com/" +
+        data.gitHub +
+        "/" +
+        data.title +
+        "/issues/" +
+        i +
+        ")"
+    );
   }
-  var str2 = issueLines + '';
+  var str2 = issueLines + "";
   var presentIssues = str2.split(",").join("\n");
 
-return `
+  return `
 #### Issues
 
 ${presentIssues}
 
-`
-}
+`;
+};
 
 // Generate Markdown for README
 function generateMarkdown(data) {
-return `
+  return `
 ## ${data.title}
 
 ${data.description}
@@ -146,14 +156,18 @@ ${variableInstallation(data.installationInstructions)}
 
 ${variableUsage(data.usageInstructions)}
 
-<h6><p align="right">:cyclone: Click the image(s) below to view the live <a  id="Screenshots" href="https://${data.gitHub}.github.io/${data.title}/">webapplication</a></p></h6>
+<h6><p align="right">:cyclone: Click the image(s) below to view the live <a  id="Screenshots" href="https://${
+    data.gitHub
+  }.github.io/${data.title}/">webapplication</a></p></h6>
 
 ${variableScreenshot1(data)}
 
 ${variableScreenshot2(data)}
 
 #### Questions
-Contact me at [${data.contact}](mailto:${data.contact}) with any questions or comments. 
+Contact me at [${data.contact}](mailto:${
+    data.contact
+  }) with any questions or comments. 
 
 #### License
 ${data.title} is published under the __${data.license}__ license.
@@ -165,20 +179,20 @@ ${licenseDetail(data.license)}
 // Write the File to Disk
 const writeFile = (fileContent) => {
   return new Promise((resolve, reject) => {
-      fs.writeFile("./dist/README.md", fileContent, (err) => {
-          // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-          if (err) {
-              reject(err);
-              // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-              return;
-          }
+    fs.writeFile("./dist/README.md", fileContent, (err) => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) {
+        reject(err);
+        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        return;
+      }
 
-          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-          resolve({
-              ok: true,
-              message: "README created!",
-          });
+      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+      resolve({
+        ok: true,
+        message: "README created!",
       });
+    });
   });
 };
 
